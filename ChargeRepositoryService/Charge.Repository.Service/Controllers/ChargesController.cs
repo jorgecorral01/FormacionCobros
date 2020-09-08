708a12c1ad168c972e0d212ebbbf1d70b54dd5f2
Jorge Corral <jorge.corralcuadrado@plexus.es>
2020-09-08 12:29:16 +0200

    add swagger to charge repository service

--- ChargeRepositoryService/Charge.Repository.Service/Controllers/ChargesController.cs
+++ ChargeRepositoryService/Charge.Repository.Service/Controllers/ChargesController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Charge.Repository.Service.Business.Dtos;
+using Charge.Repository.Service.swagger;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 
 namespace Charge.Repository.Service.Controllers {
     [Route("api/[controller]")]
     [ApiController]
     public class ChargesController : ControllerBase {
-
+        public static void Convention(ApiVersioningOptions options) {
+            options.Conventions.Controller<ChargesController>().HasApiVersions(ApiVersioning.Versions());
+        }
         // POST 
         [Route("add")]
         [HttpPost]

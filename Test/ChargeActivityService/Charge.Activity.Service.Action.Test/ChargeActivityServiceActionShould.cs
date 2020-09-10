ac6df2d47dd6127f977eb5eb4f11070266bad584
Jorge Corral <jorge.corralcuadrado@plexus.es>
2020-09-10 12:15:08 +0200

    refactor

--- Test/ChargeActivityService/Charge.Activity.Service.Action.Test/ChargeActivityServiceActionShould.cs
+++ Test/ChargeActivityService/Charge.Activity.Service.Action.Test/ChargeActivityServiceActionShould.cs
@@ -6,21 +6,31 @@ using NUnit.Framework;
 
 namespace Charge.Activity.Service.Action.Test {
     public partial class ChargeActivityServiceActionShould {
+        private static IdentifierDto identifierDto;
         [SetUp]
         public void Setup() {
+            identifierDto = GivenAnIdentifier();
         }
 
         [Test]
         public void when_we_update_activity_with_a_exist_identifier_we_obtein_true_result() {
-            var identifierDto = new IdentifierDto { identifier = "any identifier", AddResult = true } ;
-            var chargeActivityServiceRepository = Substitute.For<ChargeActivityServiceRepository>(new object[] { null });
-            chargeActivityServiceRepository.Update(identifierDto).Returns(true);
+            ChargeActivityServiceRepository chargeActivityServiceRepository = GivenArepositoryMockFor();
             var action = new UpdateActivityAction(chargeActivityServiceRepository);
 
             var result = action.Execute(identifierDto);
 
             result.Should().Be(true);
             chargeActivityServiceRepository.Received(1).Update(identifierDto);
+        }
+
+        private static ChargeActivityServiceRepository GivenArepositoryMockFor() {
+            var chargeActivityServiceRepository = Substitute.For<ChargeActivityServiceRepository>(new object[] { null });
+            chargeActivityServiceRepository.Update(identifierDto).Returns(true);
+            return chargeActivityServiceRepository;
+        }
+
+        private static IdentifierDto GivenAnIdentifier() {
+            return new IdentifierDto { identifier = "any identifier", AddResult = true };
         }
     }
 }

db0b5c0456a7adccb1149d5f1b2d5bd2b851c787
Jorge Corral <jorge.corralcuadrado@plexus.es>
2020-09-10 11:21:19 +0200

    refactor

--- Test/ChargeActivityService/Charge.Activity.Service.Repository.Test/ChargeActivityServiceRepositoryShould.cs
+++ Test/ChargeActivityService/Charge.Activity.Service.Repository.Test/ChargeActivityServiceRepositoryShould.cs
@@ -7,38 +7,51 @@ using System.Linq;
 
 namespace Charge.Activity.Service.Repository.Test {
     public class ChargeActivityServiceRepositoryShould {
-        private ChargesContext chargesContext;        
+        private ChargesContext chargesContext;
+        private IdentifierDto identifier;
+        private ChargeActivityServiceRepository chargeActivityServiceRepository;
         [SetUp]
         public void Setup() {
-            var optionsBuilder = new DbContextOptionsBuilder<ChargesContext>()
-                    .UseInMemoryDatabase(databaseName: "BDInMemory")
-                    .Options;
-            chargesContext = new ChargesContext(optionsBuilder);
+            GivenAContext();
+            GivenAActivityRepository();
+            GivenAnActivity();
         }
+     
+        [Test]
+        public void when_we_add_new_activity_we_can_rercover() {
+            var result = chargeActivityServiceRepository.Add(identifier.identifier);
+
+            VerifyResult(result);
+            DeleteActivity();
+        }       
 
         [Test]
-        public void when_we_add_new_activity_we_can_rercover() {            
-            var identifier = "any identifier";
-            var chargeActivityServiceRepository = new ChargeActivityServiceRepository(chargesContext);
-
-            var result = chargeActivityServiceRepository.Add(identifier);
-
-            result.Should().Be(true);
-            chargesContext.Activities.Select(item => item.Identifier == identifier).ToList().Should().HaveCount(1);
-            chargesContext.Activities.RemoveRange(chargesContext.Activities.Where(item => item.Identifier == identifier));
-            chargesContext.SaveChanges();
-        }
-
-        [Test]
-        public void when_we_update_activity_we_can_rercover_with_new_data() {            
-            var identifier = new IdentifierDto { identifier = "any identifier", AddResult = true };
-            var chargeActivityServiceRepository = new ChargeActivityServiceRepository(chargesContext);
+        public void when_we_update_activity_we_can_rercover_with_new_data() {                                    
             chargeActivityServiceRepository.Add(identifier.identifier);
 
             var result = chargeActivityServiceRepository.UpdateActivity(identifier);
 
+            VerifyResult(result);
+            DeleteActivity();
+        }
+        private void GivenAnActivity() {
+            identifier = new IdentifierDto { identifier = "any identifier", AddResult = true };
+        }
+        private void GivenAContext() {
+            var optionsBuilder = new DbContextOptionsBuilder<ChargesContext>()
+                                .UseInMemoryDatabase(databaseName: "BDInMemory")
+                                .Options;
+            chargesContext = new ChargesContext(optionsBuilder);
+        }
+        private void GivenAActivityRepository() {
+            chargeActivityServiceRepository = new ChargeActivityServiceRepository(chargesContext);
+        }
+        private void VerifyResult(bool result) {
             result.Should().Be(true);
             chargesContext.Activities.Select(item => item.Identifier == identifier.identifier && item.AddResult == identifier.AddResult).ToList().Should().HaveCount(1);
+        }
+
+        private void DeleteActivity() {
             chargesContext.Activities.RemoveRange(chargesContext.Activities.Where(item => item.Identifier == identifier.identifier));
             chargesContext.SaveChanges();
         }

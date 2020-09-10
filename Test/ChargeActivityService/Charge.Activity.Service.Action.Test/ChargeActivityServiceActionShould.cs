1d2b3b6a1790a121d20c5c16d637ac1708a1d8ee
Jorge Corral <jorge.corralcuadrado@plexus.es>
2020-09-10 12:10:38 +0200

    fix text

--- Test/ChargeActivityService/Charge.Activity.Service.Action.Test/ChargeActivityServiceActionShould.cs
+++ Test/ChargeActivityService/Charge.Activity.Service.Action.Test/ChargeActivityServiceActionShould.cs
@@ -14,13 +14,13 @@ namespace Charge.Activity.Service.Action.Test {
         public void when_we_update_activity_with_a_exist_identifier_we_obtein_true_result() {
             var identifierDto = new IdentifierDto { identifier = "any identifier", AddResult = true } ;
             var chargeActivityServiceRepository = Substitute.For<ChargeActivityServiceRepository>(new object[] { null });
-            chargeActivityServiceRepository.UpdateActivity(identifierDto).Returns(true);
+            chargeActivityServiceRepository.Update(identifierDto).Returns(true);
             var action = new UpdateActivityAction(chargeActivityServiceRepository);
 
             var result = action.Execute(identifierDto);
 
             result.Should().Be(true);
-            chargeActivityServiceRepository.Received(1).UpdateActivity(identifierDto);
+            chargeActivityServiceRepository.Received(1).Update(identifierDto);
         }
     }
 }

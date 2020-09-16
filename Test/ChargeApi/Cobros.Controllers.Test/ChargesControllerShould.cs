2b5c1a23894130877b104d5c82f39b1fb5c22934
Jorge Corral <jorge.corralcuadrado@plexus.es>
2020-09-16 14:45:41 +0200

    remove innecesary test

--- Test/ChargeApi/Cobros.Controllers.Test/ChargesControllerShould.cs
+++ Test/ChargeApi/Cobros.Controllers.Test/ChargesControllerShould.cs
@@ -18,7 +18,7 @@ namespace Charges.Controllers.Test {
     [TestFixture]
     public class ChargesControllerShould {
         [Test]
-        public async Task given_data_for_add_new_charge_we_obtein_an_ok_response_with_startup() {
+        public async Task given_data_for_add_new_charge_we_obtein_an_ok_response() {
             Business.Dtos.Charge newCharge = GivenANewCharge();
             HttpClient client = TestFixture.HttpClient;
             var requestUri = "http://localhost:10000/api/charges";
@@ -31,35 +31,6 @@ namespace Charges.Controllers.Test {
 
             result.StatusCode.Should().Be(HttpStatusCode.OK);
             await action.Received(1).Execute(Arg.Is<Business.Dtos.Charge>(item => item.identifier == newCharge.identifier && item.Amount == newCharge.Amount && item.Description == newCharge.Description));
-        }
-        [Test]
-        public async Task given_data_for_add_new_charge_we_obtein_an_ok_response() {
-            Business.Dtos.Charge newCharge = GivenANewCharge();
-            HttpClient client = new HttpClient();
-            var requestUri = "http://localhost:10000/api/charges";
-            var content = GivenAHttpContent(newCharge, requestUri);
-
-            var result = await client.PostAsync(requestUri, content);
-
-            result.StatusCode.Should().Be(HttpStatusCode.OK);
-        }
-
-
-
-        [Test]
-        public async Task given_a_new_charge_we_obtein_an_ok_response() {
-            var actionFactory = Substitute.For<ActionFactory>();
-            var controller = new ChargesController(actionFactory);
-            Business.Dtos.Charge charge = GivenANewCharge();
-            AddChargeAction addChargeAction = Substitute.For<AddChargeAction>(null, null);
-            addChargeAction.Execute(charge).Returns(true);
-            actionFactory.CreateAddChargeAction().Returns(addChargeAction);
-
-            var result = await controller.Post(charge);
-
-            result.Should().BeOfType<OkResult>();
-            actionFactory.Received(1).CreateAddChargeAction();
-            await addChargeAction.Received(1).Execute(charge);
         }
 
         private static Business.Dtos.Charge GivenANewCharge() {

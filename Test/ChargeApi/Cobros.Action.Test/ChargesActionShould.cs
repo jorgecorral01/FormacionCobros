f5ed9ef34b3e320edb1edab1880e7ba949ae9a98
Jorge Corral <jorge.corralcuadrado@plexus.es>
2020-09-18 09:38:56 +0200

    refactor

--- Test/ChargeApi/Cobros.Action.Test/ChargesActionShould.cs
+++ Test/ChargeApi/Cobros.Action.Test/ChargesActionShould.cs
@@ -8,31 +8,30 @@ using System.Threading.Tasks;
 
 namespace Charges.Action.Test {
     public class ChargesActionShould {
+        string identifier;
+        ChargeRepositoryServiceApiClient clientChargeRepository;
         [SetUp]
         public void Setup() {
+            clientChargeRepository = GivenARepositoryApiClientMock();
+            identifier = "any identifier";
         }
 
         [Test]
         public async Task given_data_for_add_new_charge_we_obtein_a_true_response() {
             Business.Dtos.Charge newCharge = GivenACharge();
             var addResult = true;
-            ChargeRepositoryServiceApiClient clientChargeRepository = GivenAMockRepositoryServiceClient(newCharge, addResult);
+            clientChargeRepository.AddCharge(newCharge).Returns(addResult);
             ActivityDto activityDto = GivenAnActivity(newCharge.identifier, addResult);
             ChargeActivityServiceApiClient clientActivityService = GivenAMockActivityServiceClient(activityDto);
             var addChargeAction = new AddChargeAction(clientChargeRepository, clientActivityService);
 
             var result = await addChargeAction.Execute(newCharge);
 
-            result.Should().Be(true);
-            await clientActivityService.Received(1).NotifyNewCharge(Arg.Is<ActivityDto>(item => item.identifier == activityDto.identifier));
-            await clientChargeRepository.Received(1).AddCharge(newCharge);
-            await clientActivityService.Received(1).UpdateNotifyCharge(Arg.Is<ActivityDto>(item => item.identifier == activityDto.identifier && item.AddResult == activityDto.AddResult));
+            await VerifyResult(newCharge, activityDto, clientActivityService, result);
         }
 
         [Test]
         public async Task given_an_idenfier_for_delete_charge_we_obtein_a_true_response() {
-            var identifier = "any identifier";
-            ChargeRepositoryServiceApiClient clientChargeRepository = GivenARepositoryMock();
             clientChargeRepository.DeleteCharge(identifier).Returns(true);
             var deleteChargeAction = new DeleteChargeAction(clientChargeRepository);
 
@@ -44,8 +43,6 @@ namespace Charges.Action.Test {
 
         [Test]
         public async Task given_an_idenfier_for_delete_charge_and_dont_exist_we_obtein_a_false_response() {
-            var identifier = "any identifier";
-            ChargeRepositoryServiceApiClient clientChargeRepository = GivenARepositoryMock();
             clientChargeRepository.DeleteCharge(identifier).Returns(false);
             var deleteChargeAction = new DeleteChargeAction(clientChargeRepository);
 
@@ -55,7 +52,7 @@ namespace Charges.Action.Test {
             await clientChargeRepository.Received(1).DeleteCharge(identifier);
         }
 
-        private static ChargeRepositoryServiceApiClient GivenARepositoryMock() {
+        private static ChargeRepositoryServiceApiClient GivenARepositoryApiClientMock() {
             return Substitute.For<ChargeRepositoryServiceApiClient>(new object[] { null });
         }
 
@@ -69,14 +66,14 @@ namespace Charges.Action.Test {
             return clientActivityService;
         }
 
-        private static ChargeRepositoryServiceApiClient GivenAMockRepositoryServiceClient(Business.Dtos.Charge newCharge, bool addResult) {
-            var clientChargeRepository = Substitute.For<ChargeRepositoryServiceApiClient>(new object[] { null });
-            clientChargeRepository.AddCharge(newCharge).Returns(addResult);
-            return clientChargeRepository;
+        private Business.Dtos.Charge GivenACharge() {
+            return new Charges.Business.Dtos.Charge { Description = "Nuevo cobro", Amount = 1000, identifier = identifier };
         }
-
-        private static Business.Dtos.Charge GivenACharge() {
-            return new Charges.Business.Dtos.Charge { Description = "Nuevo cobro", Amount = 1000, identifier = "anyIdentifier" };
+        private async Task VerifyResult(Business.Dtos.Charge newCharge, ActivityDto activityDto, ChargeActivityServiceApiClient clientActivityService, bool result) {
+            result.Should().Be(true);
+            await clientActivityService.Received(1).NotifyNewCharge(Arg.Is<ActivityDto>(item => item.identifier == activityDto.identifier));
+            await clientChargeRepository.Received(1).AddCharge(newCharge);
+            await clientActivityService.Received(1).UpdateNotifyCharge(Arg.Is<ActivityDto>(item => item.identifier == activityDto.identifier && item.AddResult == activityDto.AddResult));
         }
     }
 }
